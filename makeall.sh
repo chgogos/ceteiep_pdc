10acb858ee464ecca891e1e225bbe4e344dcf958
Christos Gogos <chgogos@gmail.com>
2018-04-18 01:54:18 +0300

    lab03

--- makeall.sh
+++ makeall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a arr=("lab00" "lab01" "lab01x" "lab02" "lab02x")
+declare -a arr=("lab00" "lab01" "lab01x" "lab02" "lab02x" "lab03" "lab03x")
 current_dir=$PWD
 
 for i in "${arr[@]}"
