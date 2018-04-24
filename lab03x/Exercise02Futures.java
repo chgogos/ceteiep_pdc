300820227ed56071dcb30256e0688b21c7b8ff1a
Christos Gogos <chgogos@gmail.com>
2018-04-24 23:23:30 +0300

    futures

--- lab03x/Exercise02Futures.java
+++ lab03x/Exercise02Futures.java
@@ -3,7 +3,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.locks.Lock;
 
 public class Exercise02Futures {
   static long SUM = 0;
